89df5c7544d3ca2003497e0cd35e482e071d9ab9
Daniel Kong <daniel.kong@workday.com>
2017-01-15 13:39:30 -0800

    modify
    first modified

--- StringsSeries/Repeated_Substring_Pattern.swift
+++ StringsSeries/Repeated_Substring_Pattern.swift
@@ -1,43 +1,23 @@
 // 459. Repeated Substring Pattern
 // Given a non-empty string check if it can be constructed by taking a substring of it and appending multiple copies of the substring together. You may assume the given string consists of lowercase English letters only and its length will not exceed 10000.
-// Solution: 	1. recursion. half
-//				2. two pointer
 
-// this solution get "Time Limit Exceeded"
-    func repeatedSubstringPattern(_ str: String) -> Bool {
-        let strLength = str.characters.count
-        let midIdx = strLength/2
-        if strLength == 1 {
-            return false
-        } else {
-            // rotate str
-            for i in 1...midIdx {
-                let iIndex = str.index(str.startIndex, offsetBy: i)
-                let newString = str.substring(from: iIndex) + str.substring(to: iIndex) 
-                if newString == str {
-                    return true
-                }
-            }
-            return false
-        }
-    }
-// Add if condition passed!
+// Solution: 	1. backtrack. runtime O(n^2)
+//				2. KMP O(n)     http://www.geeksforgeeks.org/find-given-string-can-represented-substring-iterating-substring-n-times/
+
 	func repeatedSubstringPattern(_ str: String) -> Bool {
         let strLength = str.characters.count
         let midIdx = strLength/2
         if strLength == 1 {
             return false
         } else {
-            // rotate str
             for i in 1...midIdx {
-                if strLength%i == 0 {
+                if strLength%i == 0 { // get "Time Limit Exceeded" if no this line
                     let iIndex = str.index(str.startIndex, offsetBy: i)
                     let newString = str.substring(from: iIndex) + str.substring(to: iIndex) 
                     if newString == str {
                         return true
                     }
                 }
-
             }
             return false
         }
